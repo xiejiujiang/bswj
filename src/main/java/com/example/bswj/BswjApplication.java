8d1c4bf7017a4e53670739ebef1c00b172fde6b8
xiejiujiang <634482891@qq.com>
2022-09-29 11:37:26 +0800

    Initial commit

---
+++ src/main/java/com/example/bswj/BswjApplication.java
@@ -0,0 +1,13 @@
+package com.example.bswj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BswjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BswjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bswj/BswjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bswj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BswjApplicationTests {
+
+    /*@Test
+    void contextLoads() {
+    }*/
+
+}
